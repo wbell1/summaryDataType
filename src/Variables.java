330fdd2c04f38397e1fea5076fcf0dc09faff068
wbell1 <106184696+wbell1@users.noreply.github.com>
2022-05-26 15:00:42 -0400

    updated variable.java

--- src/Variables.java
+++ src/Variables.java
@@ -1,6 +1,6 @@
 /**
  * class name: Variables.java
- * author: ________
+ * author: __Walker Bell______
  * date:
  * version:
  * description: This program will display summary of primitive data types.
@@ -10,11 +10,22 @@
  *            double, boolean, char (2 lines per each type)
  *
  */
-public class Variables
-{
-    public static void main(String[] args)
-    {
+public class Variables {
+    public static void main(String[] args) {
         System.out.println("Summary of The Primitive Data Types");
-    }
 
+
+        System.out.println("Byte- Has a minimum value of -128 and max of 127.");
+        System.out.println("This data type is useful for saving large amounts of memory.");
+        System.out.println("Short-This data type is 16-bit. Minimum value of -32,768 and max 32,767.");
+        System.out.println("Can be used tp save large amounts of data when you need to.");
+        System.out.println("Int- This type is 32-bit with a max value of -2^31 and a max of 2^31-1.");
+        System.out.println("Use this type as an unsigned integer. ");
+        System.out.println("Long- This is a 64-bit data type with a min value of -2^63 and max of 2^63-1.");
+        System.out.println("In Java SE 8 or later use this to represent an unsigned 64-bit long and when you need a range or values wider than int.");
+        System.out.println("Float is a single precision 32-bit. Use it to save large amount of floating point numbers.");
+        System.out.println("Double is a double precision 64-bit floating point. Usually a defualt choice.");
+        System.out.println("Boolean has only two values, true and false. Use this for conditions with true or false.");
+        System.out.println("Char is a single 16-bit unicode character and a min valuse of u000 and max of ufff.");
+    }
 }
